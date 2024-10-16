fa5576b459ad089fd94f03962e54a9e92534a597
STH <sth6197@gmail.com>
2024-10-16 19:46:43 +0900

    (2024.10.16) 플레이팹 SDK 설치

---
+++ Photon Network Game/Assets/Scripts/PlayfabManager.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using Photon.Pun;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.UI;
+
+public class PlayfabManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] InputField emailInputField;
+    [SerializeField] InputField passwordInputField;
+
+    public void Success(LoginResult loginResult)
+    {
+        PhotonNetwork.AutomaticallySyncScene = false;
+
+        PhotonNetwork.GameVersion = "1.0f";
+
+        PhotonNetwork.LoadLevel("Lobby Scene");
+    }
+
+    public void Success(RegisterPlayFabUserResult registerPlayFabUserResult)
+    {
+        Debug.Log(registerPlayFabUserResult.Request);
+        Debug.Log(registerPlayFabUserResult.ToString());
+    }
+
+
+    public void Failure(PlayFabError playFabError)
+    {
+        Debug.Log(playFabError.GenerateErrorReport());
+    }
+
+    public void OnSignIn()
+    {
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = emailInputField.text,
+            Password = passwordInputField.text
+        };
+
+        PlayFabClientAPI.LoginWithEmailAddress
+        (
+            request,
+            Success,
+            Failure
+        );
+    }
+
+    public void OnSignUp()
+    {
+        var request = new RegisterPlayFabUserRequest
+        {
+            Email = emailInputField.text,
+            Password = passwordInputField.text,
+            RequireBothUsernameAndEmail = false
+        };
+
+        PlayFabClientAPI.RegisterPlayFabUser
+        (
+            request,
+            Success,
+            Failure
+        );
+    }
+}
